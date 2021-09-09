b853fd49a50bde5e67d4db797c2484260254fc4d
vandenman <donvdbergh@hotmail.com>
2021-09-09 15:53:39 +0200

    add custom abline

---
+++ R/geom_abline2.R
@@ -0,0 +1,118 @@
+#' Add an abline that does not exceed the extrema of the axes breaks.
+#'
+#' @description
+#' The default abline of ggplot2 will exceeds the breaks and goes from one set of limits to the next.
+#' This is a modified verions that respect one will not exceed the extrema of the x and y-breaks.
+#'
+#' @inheritParams ggplot2::geom_abline
+#' @param method Either "breaks" (default) to respect the extrema of the axes or "ggplot2" to obtain the ggplot2 behavior.
+#'
+#' @return
+#' @example inst/examples/ex-geom_abline2.R
+#' @export
+geom_abline2 <- function(mapping = NULL, data = NULL, method = c("breaks", "ggplot2"), ..., slope, intercept,
+                          na.rm = FALSE, show.legend = NA) {
+
+  method <- match.arg(method)
+  if (is.null(mapping) && missing(slope) && missing(intercept)) {
+    slope <- 1
+    intercept <- 0
+  }
+  if (!missing(slope) || !missing(intercept)) {
+    if (missing(slope))
+      slope <- 1
+    if (missing(intercept))
+      intercept <- 0
+    data <- data.frame(intercept = intercept, slope = slope, method = method)
+    mapping <- ggplot2::aes(intercept = intercept, slope = slope, method = method)
+    show.legend <- FALSE
+  }
+  layer(data = data, mapping = mapping, stat = ggplot2::StatIdentity,
+        geom = GeomAbline2, position = ggplot2::PositionIdentity, show.legend = show.legend,
+        inherit.aes = FALSE, params = list(na.rm = na.rm, ...))
+}
+
+#' @rdname geom_abline2
+#' @usage NULL
+#' @format NULL
+#' @export
+#' @export
+GeomAbline2 <- ggplot2::ggproto(
+  "abline2",
+  ggplot2::GeomAbline,
+  required_aes = c("slope", "intercept"),
+  optional_aes = "method",
+
+  draw_panel = function(data, panel_params, coord, parse = FALSE,
+                        na.rm = FALSE, check_overlap = FALSE, prepend = NULL) {
+
+    if (is.null(data$method)) {
+      idxBreaks <- seq_len(nrow(data))
+      idxGGplot2 <- integer()
+    } else {
+      if (!all(data$method %in% c("breaks", "ggplot2")))
+        stop("aestethic 'method' for geom_abline2 expects either 'breaks' or 'ggplot2' but got something else!")
+
+      idxBreaks  <- which(data$method == "breaks")
+      idxGGplot2 <- which(data$method == "ggplot2")
+    }
+
+    if (length(idxBreaks) > 0) {
+      ranges <- list(
+        x = range(panel_params$x$get_breaks(), na.rm = TRUE),
+        y = range(panel_params$y$get_breaks(), na.rm = TRUE)
+      )
+
+      data$x   [idxBreaks] <- ranges$x[1]
+      data$xend[idxBreaks] <- ranges$x[2]
+
+      data$y   [idxBreaks] <- ranges$x[1] * data$slope[idxBreaks] + data$intercept[idxBreaks]
+      data$yend[idxBreaks] <- ranges$x[2] * data$slope[idxBreaks] + data$intercept[idxBreaks]
+
+      # for all y and yend, check if they exceed the lower bound or upper bound of ranges$y.
+      # if they do we change y = ax + b to x = (y - b) / a
+      idxNeedsFlip <- idxBreaks[data$y[idxBreaks] < ranges$y[1] | data$y[idxBreaks] > ranges$y[2]]
+      if (length(idxNeedsFlip) > 0) {
+        data$y[idxNeedsFlip] <-  ranges$y[1]
+        data$x[idxNeedsFlip] <- (ranges$y[1] - data$intercept[idxNeedsFlip]) / data$slope[idxNeedsFlip]
+      }
+
+      idxNeedsFlip <- idxBreaks[data$y[idxBreaks] > ranges$y[2]]
+      if (length(idxNeedsFlip) > 0) {
+        data$y[idxNeedsFlip] <-  ranges$y[2]
+        data$x[idxNeedsFlip] <- (ranges$y[2] - data$intercept[idxNeedsFlip]) / data$slope[idxNeedsFlip]
+      }
+
+      idxNeedsFlip <- idxBreaks[data$yend[idxBreaks] < ranges$y[1]]
+      if (length(idxNeedsFlip) > 0) {
+        data$yend[idxNeedsFlip] <-  ranges$y[1]
+        data$xend[idxNeedsFlip] <- (ranges$y[1] - data$intercept[idxNeedsFlip]) / data$slope[idxNeedsFlip]
+      }
+
+      idxNeedsFlip <- idxBreaks[data$yend[idxBreaks] > ranges$y[2]]
+      if (length(idxNeedsFlip) > 0) {
+        data$yend[idxNeedsFlip] <-  ranges$y[2]
+        data$xend[idxNeedsFlip] <- (ranges$y[2] - data$intercept[idxNeedsFlip]) / data$slope[idxNeedsFlip]
+      }
+
+    }
+
+    if (length(idxGGplot2) > 0) {
+
+      ranges <- coord$backtransform_range(panel_params)
+      if (coord$clip == "on" && coord$is_linear()) {
+        ranges$x <- ranges$x + c(-1, 1) * diff(ranges$x)
+      }
+
+      data$x   [idxGGplot2] <- ranges$x[1]
+      data$xend[idxGGplot2] <- ranges$x[2]
+      data$y   [idxGGplot2] <- ranges$x[1] * data$slope[idxGGplot2] + data$intercept[idxGGplot2]
+      data$yend[idxGGplot2] <- ranges$x[2] * data$slope[idxGGplot2] + data$intercept[idxGGplot2]
+
+    }
+
+    GeomSegment$draw_panel(unique(data), panel_params, coord)
+
+  }
+)
+
---
+++ inst/examples/ex-geom_abline2.R
@@ -0,0 +1,27 @@
+library(ggplot2)
+library(jaspGraphs)
+
+dfRect <- data.frame(xmin = -10, ymin = -10, xmax = 10, ymax = 10)
+basePlot <- ggplot(data = dfRect, aes(xmin = xmin, ymin = ymin, ymax = ymax, xmax = xmax)) +
+  geom_rect(fill = NA, color = "black") +
+  geom_rangeframe() +
+  themeJaspRaw(legend.position = "right")
+
+slopes <- seq(-5, 5, length.out = 15)
+ints   <- seq(-2, 2, length.out = length(slopes))
+basePlot +
+  geom_abline2(intercept = ints - 2, slope = slopes, method = "breaks",  color = "green") +
+  geom_abline2(intercept = ints + 2, slope = slopes, method = "ggplot2", color = "red") +
+  ggtitle("specify 'method' manually")
+
+dfAbline <- data.frame(
+  intercept = -9:8,
+  slope = 0.35,
+  method = rep(c("ggplot2", "breaks"), 9)
+)
+
+basePlot + geom_abline2(data = dfAbline, mapping = aes(intercept = intercept, slope = slope, method = method, color = method), show.legend = TRUE) +
+  ggtitle("specify 'method' through aes")
+
+basePlot + geom_abline2(data = dfAbline, mapping = aes(intercept = intercept, slope = slope, color = method)) +
+  ggtitle("if method is not specified, 'breaks' is used as a default")
