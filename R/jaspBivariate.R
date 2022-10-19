09f78564ffbb4a69d0b4112061b34382163bf501
Kucharssim <kucharssim@gmail.com>
2022-10-19 14:15:06 +0200

    rename jaspScatter to jaspBivariate

--- R/jaspBivariate.R
+++ R/jaspBivariate.R
@@ -1,8 +1,8 @@
-#' @title Scatter plots with optional confidence and prediction intervals.
+#' @title Bivariate plots with optional confidence and prediction intervals.
 #'
 #' @description This plot consists of three layers:
 #' \enumerate{
-#'   \item The distribution of the data displayed as [geom_point] or as a [ggplot2::geom_hex].
+#'   \item The bivariate distribution.
 #'   \item Smooth line through the data displayed using [ggplot2::geom_smooth].
 #'   \item Prediction interval of y given x using [stats::predict.lm](assuming linear relationship), or prediction ellipse assuming bivariate normal distribution.
 #' }
@@ -15,32 +15,32 @@
 #' \describe{
 #'    \item{"point"}{Using [geom_point].}
 #'    \item{"hex"}{Using [ggplot2::geom_hex].}
-#'    \item{"contour"}{Using [ggplot2::geom_contour].}
+#'    \item{"bin"}{Using [ggplot2::geom_bin2d].}
+#'    \item{"contour"}{Using [ggplot2::geom_density2d].}
+#'    \item{"density"}{Using [ggplot2::geom_density2d_filled].}
 #' }
-#' @param bins,binwidth Arguments passed to [ggplot2::geom_hex].
-#' @param palette Argument passed to [JASPcolors]. Palette to use for drawing [ggplot2::geom_hex] and [ggplot2::geom_contour].
-#' @param fill Argument passed to [geom_point].
-#' @param alpha Argument passed to [geom_point] or [ggplot2::geom_hex].
+#' @param args A list of additional arguments passed to the geom function determined by \code{type} argument.
 #' @param smooth Character; passed as \code{method} argument to [ggplot2::geom_smooth],
-#' unless \code{smooth == "none"}, in which case the layer si not plotted.
+#' unless \code{smooth == "none"}, in which case the layer is not plotted.
 #' @param smoothCi Logical; Should confidence interval around the smooth line be plotted?
 #' Passed as \code{se} argument to [ggplot2::geom_smooth].
 #' @param smoothCiLevel Numeric; Confidence level of the confidence interval around the smooth line.
 #' Passed as \code{level} argument to [ggplot2::geom_smooth].
-#' @param smoothColor Color of the smooth line.
+#' @param smoothArgs A list of additional arguments passed to [ggplot2::geom_smooth].
 #' @param predict Character; Method for drawing the prediction interval:
 #' \describe{
 #'   \item{"none"}{Prediction interval is not displayed.}
 #'   \item{"lm"}{Prediction interval is plotted, the confidence bands are calculated using [stats::predict.lm].}
-#'   \item{"ellipse"}{Prediction ellipse is plotted, assuming bi-variate normal model.}
+#'   \item{"ellipse"}{Prediction ellipse is plotted using [ggplot2::stat_ellipse].}
 #' }
-#' @param predictCiLevel Numeric; Confidence level of the prediction interval.
-#' @param predictColor Color of the prediction interval.
-#' @param suppressAxesLabels Logical; should axis labels be suppressed.
+#' @param predictLevel Numeric; Confidence level of the prediction interval.
+#' @param predictArgs A list of additional arguments passed to the function that draws the prediction interval.
+#' @param xBreaks Optional numeric vector that specifies the breaks along the x-axis.
+#' @param yBreaks Optional numeric vector that specifies the breaks along the y-axis.
 #' @export
-jaspScatter <- function(
+jaspBivariate <- function(
     x, y, group = NULL, xName, yName,
-    type               = c("point", "hex", "bin", "density", "contour"),
+    type               = c("point", "hex", "bin", "contour", "density"),
     args               = list(color = "black"),
     smooth             = c("none", "lm", "glm", "gam", "loess"),
     smoothCi           = FALSE,
@@ -49,7 +49,6 @@ jaspScatter <- function(
     predict            = c("none", "lm", "ellipse"),
     predictCiLevel     = 0.95,
     predictArgs        = list(),
-    suppressAxesLabels = FALSE,
     xBreaks            = NULL,
     yBreaks            = NULL
 ) {
@@ -81,8 +80,8 @@ jaspScatter <- function(
     point   = jaspGraphs::geom_point,
     hex     = ggplot2::geom_hex,
     bin     = ggplot2::geom_bin2d,
-    density = ggplot2::geom_density2d,
-    contour = ggplot2::geom_density2d_filled
+    contour = ggplot2::geom_density2d,
+    density = ggplot2::geom_density2d_filled
   )
   baseLayer <- do.call(baseGeom, args)
 
@@ -163,17 +162,42 @@ jaspScatter <- function(
   return(plot)
 }
 
-jaspScatterWithMargins <- function(
+#' @title Bivariate plots with marginal distributions along the axes.
+#'
+#' @description This plot consists of four elements:
+#' \enumerate{
+#'   \item The bivariate plot of \code{x} and \code{y} in the bottom-left panel displayed by [jaspBivariate].
+#'   \item Marginal distributions along the diagonal displayed by [jaspHistogram]. The plot on the bottom-right has transposed axes.
+#'   \item (Optional) custom plot on the top-right panel. See details.
+#' }
+#'
+#' @param x Numeric vector of values on the x-axis.
+#' @param y Numeric vector of values on the y-axis.
+#' @param group Optional grouping variable.
+#' @param xName Character; x-axis label. If left empty, the name of the \code{x} object is displayed. To remove the axis label, use \code{NULL}.
+#' @param yName Character; y-axis label. If left empty, the name of the \code{y} object is displayed. To remove the axis label, use \code{NULL}.
+#' @param margins Numeric vector; The proportions of the subplots relative to each other.
+#' @param binWidthType See [jaspHistogram]. Used for determining consistent axes for the bivariate and marginal distribution plots.
+#' @param numberOfBins See [jaspHistogram]. Used for determining consistent axes for the bivariate and marginal distribution plots.
+#' @param histogramArgs An optional list of options passed to [jaspHistogram].
+#' @param topRightPlotFunction An optional function that can be used to plotting something in the top-right panel. If \code{NULL} (default), an empty area is plotted.
+#' @param topRightPlotArgs An optional list of options passed to \code{topRightPlotFunction}.
+#' @param ... Additional options passed to [jaspBivariate].
+#'
+#' @export
+jaspBivariateWithMargins <- function(
   x, y, group = NULL, xName, yName, margins = c(0.25, 0.75),
   binWidthType = c("doane", "fd", "scott", "sturges", "manual"), numberOfBins = NA,
   histogramArgs = list(density = TRUE),
+  topRightPlotFunction = NULL,
+  topRightPlotArgs = list(),
   ...
   ) {
 
   xBreaks <- getJaspHistogramBreaks(x = x, binWidthType = binWidthType, numberOfBins = numberOfBins)
   yBreaks <- getJaspHistogramBreaks(x = y, binWidthType = binWidthType, numberOfBins = numberOfBins)
 
-  bottomLeft <- jaspScatter(x = x, y = y, group = group, xName = xName, yName = yName, xBreaks = xBreaks, yBreaks = yBreaks, ...)
+  bottomLeft <- jaspBivariate(x = x, y = y, group = group, xName = xName, yName = yName, xBreaks = xBreaks, yBreaks = yBreaks, ...)
 
   histogramArgs[["binWidthType"]] <- binWidthType
   histogramArgs[["numberOfBins"]] <- numberOfBins
@@ -200,7 +224,13 @@ jaspScatterWithMargins <- function(
     ggplot2::coord_flip()
 
 
-  topRight <- patchwork::plot_spacer()
+  if (is.function(topRightPlotFunction) && is.list(topRightPlotArgs)) {
+    topRightPlotArgs[["x"]]     <- x
+    topRightPlotArgs[["y"]]     <- y
+    topRight <- do.call(topRightPlotFunction, topRightPlotArgs)
+  } else if (is.null(topRightPlotFunction)) {
+    topRight <- patchwork::plot_spacer()
+  }
 
   patchwork::wrap_plots(
     topLeft, topRight, bottomLeft, bottomRight,
