509ec8ac021aca8ab69a71921784b1af0ab3bd1c
Johnny <j.b.vandoorn@uva.nl>
2024-04-10 15:02:55 +0200

    same order

--- R/plotQQnorm.R
+++ R/plotQQnorm.R
@@ -20,7 +20,7 @@
 #' jaspGraphs::plotQQnorm(x, lower, upper)
 #'
 #' @export
-plotQQnorm <- function(residuals, lower = NULL, upper = NULL, abline = TRUE, ablineColor = "red", ablineOrigin = FALSE, identicalAxes = FALSE, 
+plotQQnorm <- function(residuals, lower = NULL, upper = NULL, abline = TRUE, ablineOrigin = FALSE, ablineColor = "red", identicalAxes = FALSE, 
                        xName = gettext("Theoretical quantiles",domain="R-jaspGraphs"), yName = gettext("Observed quantiles",domain="R-jaspGraphs")) {
 
   n <- length(residuals)
