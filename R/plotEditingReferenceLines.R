c90abf48f649037168dc6256d3c9157c3da014f3
Don van den Bergh <donvdbergh@hotmail.com>
2026-04-10 15:26:16 +0200

    add extra stuff for reference lines

--- R/plotEditingReferenceLines.R
+++ R/plotEditingReferenceLines.R
@@ -36,34 +36,66 @@ referenceToGeom <- function(references) {
 
 referenceToHLine <- function(reference) {
   # ggplot2::geom_hline(yintercept = reference$y)
+  color    <- reference$color    %||% "black"
+  lwd      <- reference$linewidth %||% 1
+  lty      <- linetypeFromInt(reference$linetype %||% 0L)
   df <- data.frame(x = I(.985), y = reference$y, text = reference$text)
   if (reference$text == "")
-    return(ggplot2::geom_hline(data = df, mapping = aes(yintercept = y)))
+    return(ggplot2::geom_hline(data = df, mapping = aes(yintercept = y), colour = color, linewidth = lwd, linetype = lty))
   else return(list(
-    ggplot2::geom_hline(data = df, mapping = aes(yintercept = y)),
+    ggplot2::geom_hline(data = df, mapping = aes(yintercept = y), colour = color, linewidth = lwd, linetype = lty),
     ggplot2::geom_label(data = df, mapping = aes(x = x, y = y, label = text), hjust = "inward")
   ))
 }
 
 referenceToVLine <- function(reference) {
-  ggplot2::geom_vline(xintercept = reference$x)
+  color    <- reference$color    %||% "black"
+  lwd      <- reference$linewidth %||% 1
+  lty      <- linetypeFromInt(reference$linetype %||% 0L)
   df <- data.frame(x = reference$x, y = I(.985), text = reference$text)
   if (reference$text == "")
-    return(ggplot2::geom_vline(data = df, mapping = aes(xintercept = x)))
+    return(ggplot2::geom_vline(data = df, mapping = aes(xintercept = x), colour = color, linewidth = lwd, linetype = lty))
   else return(list(
-    ggplot2::geom_vline(data = df, mapping = aes(xintercept = x)),
-    ggplot2::geom_label(data = df, mapping = aes(x = x, y = y, label = text), hjust = "inward", angle = 90)
+    ggplot2::geom_vline(data = df, mapping = aes(xintercept = x), colour = color, linewidth = lwd, linetype = lty),
+    ggplot2::geom_label(data = df, mapping = aes(x = x, y = y, label = text), hjust = "inward")
   ))
 }
 
 referenceToPoint <- function(reference) {
+  color <- reference$color     %||% "black"
+  sz    <- reference$linewidth %||% 1
+  shp   <- shapeFromInt(reference$linetype %||% 0L)
   df <- data.frame(x = reference$x, y = reference$y, text = reference$text)
   if (reference$text == "")
-    return(ggplot2::geom_point(data = df, mapping = aes(x = x, y = y)))
+    return(ggplot2::geom_point(data = df, mapping = aes(x = x, y = y), colour = color, size = sz, shape = shp))
   else return(list(
-    ggplot2::geom_point(data = df, mapping = aes(x = x, y = y)),
+    ggplot2::geom_point(data = df, mapping = aes(x = x, y = y), colour = color, size = sz, shape = shp),
     ggplot2::geom_text(data = df, mapping = aes(x = x, y = y, label = text), hjust = 0, nudge_x = 0.05)
   ))
+}
+
+shapeFromInt <- function(i) {
+  switch(as.character(i),
+    "0" = 16L,  # filled circle
+    "1" = 15L,  # filled square
+    "2" = 17L,  # filled triangle
+    "3" = 18L,  # filled diamond
+    "4" = 3L,   # cross / plus
+    "5" = 8L,   # star / asterisk
+    16L
+  )
+}
+
+linetypeFromInt <- function(i) {
+  switch(as.character(i),
+    "0" = "solid",
+    "1" = "dashed",
+    "2" = "dotted",
+    "3" = "dotdash",
+    "4" = "longdash",
+    "5" = "twodash",
+    "solid"
+  )
 }
 
 referencesToNames <- function(references) {
